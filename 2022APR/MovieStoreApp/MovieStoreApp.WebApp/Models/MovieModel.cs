a6b4f67f993f319f15f7c4786c5cca5ae248f4fe
Catherine <princess.corn@gmail.com>
2022-04-13 08:49:20 -0500

    Add basic list of all movies without database

---
+++ 2022APR/MovieStoreApp/MovieStoreApp.WebApp/Models/MovieModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieStoreApp.WebApp.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string PosterUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
