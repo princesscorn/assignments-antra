f8be2374cb278d05894796775db5c2915521c082
Catherine <princess.corn@gmail.com>
2022-04-12 23:05:18 -0500

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        /* create a host */
+        public static IHostBuilder CreateHostBuilder (string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(
+                webBuilder => { webBuilder.UseStartup<Startup>(); }
+                );
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    public class Startup
+    {
+        // We will use this method to do the denpendency injection
+        public void ConfigureServices (IServiceCollection service)
+        {
+        }
+        // It will be used to work with middleware and environment related thing
+        // development env/testing(QA/stagging) env/production env
+        public void Configure (IApplicationBuilder appBuilder, IWebHostEnvironment env)
+        {
+            appBuilder.UseRouting ();   // Middleware
+
+            appBuilder.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Congratulations! Convention from Console App to Web App!");
+                });
+            });
+        }
+    }
+}
