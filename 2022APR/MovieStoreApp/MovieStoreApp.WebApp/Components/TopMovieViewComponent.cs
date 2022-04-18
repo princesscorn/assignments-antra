d75216a2b8ea8754d1b36c378a37ab369b3cbf05
Catherine <princess.corn@gmail.com>
2022-04-18 00:17:23 -0500

    Partial and ViewComponent

---
+++ 2022APR/MovieStoreApp/MovieStoreApp.WebApp/Components/TopMovieViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreApp.Core.Contract.Service;
+
+namespace MovieStoreApp.WebApp.Components
+{
+    public class TopMovieViewComponent : ViewComponent
+    {
+        IMovieServiceAsync movieService;
+
+        public TopMovieViewComponent(IMovieServiceAsync srv)
+        {
+            movieService = srv;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await movieService.GetTop10RevenueMoviesAsync();
+            return View(result);
+        }
+     }
+}
